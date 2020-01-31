578749926ac181a85e5027d2946eb73c5de74cb7
Snep <snep@snep.pw>
2020-01-30 19:56:05 -0800

    event start/end times

--- botbase.py
+++ botbase.py
@@ -18,22 +18,51 @@ with open(os.path.join(os.path.dirname(__file__), "channels.json"), "r") as botc
     BOT_CHANNELS = json.loads(botchannels.read())
 
 _id = 0
-EVENTS = []
-ACTIVE_EVENTS = []
+EVENTS = set()
+ACTIVE_EVENTS = set()
+
+
+class EventDate:
+    def __init__(self, datestring):
+        self.datestring = datestring
+        date, time = datestring.split(" ")
+        day, mon = date.split("/")
+        hour, minute = time.split(":")
+        self.day = int(day)
+        self.mon = int(mon)
+        self.hour = int(hour)
+        self.minute = int(minute)
+
+    def to_datetime(self, tz, year):
+        date = datetime.datetime.strptime(self.datestring, "%d/%m %H:%M")
+        date = date.replace(year=year, tzinfo=tz)
+        return date
 
 
 class Event:
-    def __init__(self, name, datestring, asset, timezone, event_type):
-        date = datestring.split("/")
+    def __init__(self, name, startdate, enddate, asset, timezone, event_type):
         global _id
         self.id = _id
         _id += 1
-        self.day = int(date[0])
-        self.mon = int(date[1])
+        self.start_date = startdate
+        self.end_date = enddate
         self.type = event_type
         self.asset = asset
         self.name = name
         self.timezone = pytz.timezone(BOT_EVENTS['timezones'][timezone])
+
+    def is_active(self, current_time=None):
+        if not current_time:
+            current_time = datetime.datetime.now(tz=self.timezone)
+        start = self.start_date.to_datetime(self.timezone, current_time.year)
+        end = self.end_date.to_datetime(self.timezone, current_time.year)
+        print(start, end, current_time)
+        if (start < end):
+            # event does not wrap a year
+            return current_time >= start and current_time < end
+        else:
+            # event wraps a year
+            return start >= current_time or end < current_time
 
     @staticmethod
     def get_by_id(id):
@@ -42,8 +71,10 @@ class Event:
 
 for k, v in BOT_EVENTS['events'].items():
     for e in v:
-        EVENTS.append(Event(e['name'], e['date'],
-                            e['asset'], e['timezone'], k))
+        start = EventDate(e['start'])
+        end = EventDate(e['end'])
+        EVENTS.add(Event(e['name'], start, end,
+                         e['asset'], e['timezone'], k))
 
 TIMEZONES = BOT_EVENTS['timezones']
 
@@ -65,32 +96,39 @@ async def check_task():
     initial_startup = True  # don't announce events have started on the first run of the loop
     _daycount = 1
     while not bot.is_closed():
-        current_active = []  # the active events in this run of the loop
+        current_active = set()  # the active events in this run of the loop
         print(_daycount)
+
+        to_announce_start = []
+        to_announce_end = []
 
         # find which events are active
         for ev in EVENTS:
-            date = datetime.datetime.now(tz=ev.timezone).date()
-            if ev.mon == date.month and ev.day == _daycount:
+            date = datetime.datetime.now(tz=ev.timezone)
+            date = date.replace(day=_daycount)
+            if ev.is_active(date):
                 # event is active
 
-                current_active.append(ev.id)
+                current_active.add(ev.id)
                 if ev.id not in ACTIVE_EVENTS:
                     # event has just started
 
-                    ACTIVE_EVENTS.append(ev.id)
-                    if not initial_startup:
-                        await announce_event_start(ev)
+                    ACTIVE_EVENTS.add(ev.id)
+                    to_announce_start.append(ev)
 
         # find events that just ended
-        for eid in ACTIVE_EVENTS:
+        for eid in set(ACTIVE_EVENTS):
             if eid not in current_active:
                 # event has just ended
 
                 ACTIVE_EVENTS.remove(eid)
-                if not initial_startup:
-                    ev = Event.get_by_id(eid)
-                    await announce_event_end(ev)
+                ev = Event.get_by_id(eid)
+                to_announce_end.append(ev)
+
+        if not initial_startup:
+            tasks = [announce_event_start(
+                ev) for ev in to_announce_start] + [announce_event_end(ev) for ev in to_announce_end]
+            asyncio.gather(*tasks)
 
         print(ACTIVE_EVENTS)
         _daycount %= 30
